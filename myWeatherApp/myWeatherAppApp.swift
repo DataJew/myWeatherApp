33b2924212a9e8692fdf08d0843618580ebce6e0
Justin Holt <86810022+DataJew@users.noreply.github.com>
2022-03-29 18:47:17 -0400

    Add files via upload

---
+++ myWeatherApp/myWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myWeatherAppApp.swift
+//  myWeatherApp
+//
+//  Created by Justin Holt on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
